a3e2ce02add5e8b7069ee706d0f4d7b07b000fd8
Easton Potokar <contagon6@gmail.com>
2020-10-28 00:36:47 -0600

    Setup option to enable/disable parallel jit

--- systems/nonlinear.py
+++ systems/nonlinear.py
@@ -5,7 +5,7 @@ import sympy as sy
 from systems import mvn
 
 class DiscreteNonlinearSystem:
-    def __init__(self, f, h, R, Q):
+    def __init__(self, f, h, R, Q, var=None, setup_parallel=False):
         self.n = len(R)
         self.m = len(Q)
         self.R = R
@@ -13,16 +13,23 @@ class DiscreteNonlinearSystem:
 
         # convert sympy functions into jitted functions
         # we use sympy so it can be used with EKF/Jacobians later
-        var = [f"x{i}" for i in range(self.n)]
+        if var is None:
+            self.var = [f"x{i}" for i in range(self.n)]
+        else:
+            self.var = var
         self.f_sym = f
-        self.f = njit( sy.lambdify([var], f, 'numpy') )
         self.h_sym = h
-        self.h = njit( sy.lambdify([var], h, 'numpy') )
+        self.f = sy.lambdify([self.var], f, 'numpy')
+        self.h = sy.lambdify([self.var], h, 'numpy')
 
         # setup the parrallelized datagen function
-        self.setup_data_gen()
+        self.parallel = setup_parallel
+        if setup_parallel:
+            self.f = njit(self.f)
+            self.h = njit(self.h)
+            self.setup_parallel()
 
-    def setup_data_gen(self):
+    def setup_parallel(self):
         f = self.f
         h = self.h
         n = self.n
@@ -30,32 +37,45 @@ class DiscreteNonlinearSystem:
         L = np.linalg.cholesky(self.R)
         P = np.linalg.cholesky(self.Q)
 
-        @guvectorize(["f8[:], i8[:], i8[:], f8[:,:], f8[:,:]"], 
-                            "(n),(t),(m) -> (t,n),(t,m)", 
+        @guvectorize(["f8[:], i8[:], i8[:], boolean, f8[:,:], f8[:,:]"], 
+                            "(n),(t),(m),() -> (t,n),(t,m)", 
                                 nopython=True, target='parallel')
-        def gen_data_parallel(x0, t, m_array, x, z):
-            x[0] = x0
-            z[0] = np.array(h(x0)) + mvn(mean=np.zeros(m), sqrtcov=P)
-            for i in t:
-                if i == 0:
-                    continue
-                x[i] = np.array(f(x[i-1])) + mvn(mean=np.zeros(n), sqrtcov=L)
-                z[i] = np.array(h(x[i])) + mvn(mean=np.zeros(m), sqrtcov=P)
-        
-        @guvectorize(["f8[:], i8[:], i8[:], f8[:,:], f8[:,:]"], 
-                            "(n),(t),(m) -> (t,n),(t,m)", 
-                                nopython=True, target='parallel')
-        def gen_data_parallel_nonoise(x0, t, m_array, x, z):
+        def gen_data_parallel(x0, t, m_array, noise, x, z):
             x[0] = x0
             z[0] = np.array(h(x0))
+            if noise:
+                z[0] += mvn(mean=np.zeros(m), sqrtcov=P)
             for i in t:
                 if i == 0:
                     continue
-                x[i] = np.array(f(x[i-1])) 
-                z[i] = np.array(h(x[i]))
+                if noise:
+                    x[i] = np.array(f(x[i-1])) + mvn(mean=np.zeros(n), sqrtcov=L)
+                    z[i] = np.array(h(x[i])) + mvn(mean=np.zeros(m), sqrtcov=P)
+                else:
+                    x[i] = np.array(f(x[i-1]))
+                    z[i] = np.array(h(x[i]))
+
 
         self._gen_data_parallel = gen_data_parallel
-        self._gen_data_parallel_nonoise = gen_data_parallel_nonoise
+
+    def _gen_data_single(self, x0, t, L, P, noise=True):
+        x = np.zeros((t, self.n))
+        z = np.zeros((t, self.m))
+        x[0] = x0
+        z[0] = np.array(self.h(x0))
+        if noise:
+            z[0] += mvn(mean=np.zeros(self.m), sqrtcov=P)
+        for i in range(1,t):
+            if noise:
+                x[i] = np.array(self.f(x[i-1])) + mvn(mean=np.zeros(self.n), sqrtcov=L)
+                z[i] = np.array(self.h(x[i])) + mvn(mean=np.zeros(self.m), sqrtcov=P)
+            else:
+                x[i] = np.array(self.f(x[i-1]))
+                z[i] = np.array(self.h(x[i]))
+        return x, z
+
+        
+
 
     def gen_data(self, x0, t, noise=True):
         """Wrapper to call parallelized version a little more intuitively
@@ -68,10 +88,24 @@ class DiscreteNonlinearSystem:
         Returns:
             x: generated data, size (N,t,n)
             z: generated measurements, size (N,t,m)"""
-        if noise:
-            return self._gen_data_parallel(x0, np.arange(t), np.arange(self.m))
+
+        if self.parallel:
+            return self._gen_data_parallel(x0, np.arange(t), np.arange(self.m), noise)
         else:
-            return self._gen_data_parallel_nonoise(x0, np.arange(t), np.arange(self.m))
+            L = np.linalg.cholesky(self.R)
+            P = np.linalg.cholesky(self.Q)
+
+            #determine if we need to loop over multiple starting conditions
+            if len(x0.shape) > 1:
+                x = np.zeros((x0.shape[0], t, self.n))
+                z = np.zeros((x0.shape[0], t, self.m))
+                for i, x1 in enumerate(x0):
+                    x[i], z[i] = self._gen_data_single(x1, t, L, P, noise)
+
+                return x, z
+
+            else:
+                return self._gen_data_single(x0, t, L, P, noise)
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -16,21 +16,19 @@ h = sy.Array([x0**2 + x1*5])
 
 R = np.array([[.0001, 0],
                 [0, .0001]])
-Q = np.array([[.01]])
+Q = np.array([[.1]])
 
 #make system and gather data
-sys = DiscreteNonlinearSystem(f, h, R, Q)
-
 t = 100
-x0 = np.array([1,0])
-x, z = sys.gen_data(x0, t)
+x0 = np.array([2,0])
 
+sys = DiscreteNonlinearSystem(f, h, R, Q, setup_parallel=True)
+x, z = sys.gen_data(x0, t)
 
 #Run it through the ekf
 sigma = np.eye(2)
 ekf = ExtendKalmanFilter(sys, x0, sigma)
 x_clean, sigmas = ekf.iterate(z[1:])
-print(x_clean.shape)
 
 plt.plot(np.arange(t)*dt, x.T[0], label="OG Data")
 plt.plot(np.arange(t)*dt, x_clean.T[0], label="EKF Filtered Data")
